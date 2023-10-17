eaea1120df710e305f43d1ebef1b7897e3dfc015
szychowskim <szychowski@educ.college-montreal.qc.ca>
2023-10-17 11:10:50 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#créé par mikolai Szychowski
+#cree le 18 octobre 2023
+#TP3 combat de monstres
+def jeu():
+
+    niveau_vie = 20
+    victoire = 0
+    défaite = 0
+
+
+
+    print('Bonjour, bienvenu dans ce jeu ou vous combattrez des monstres')
+    nom=str(input('Quelle sera votre nom?'))
+    print('Bonne chance',nom,'.')
+
+    while True:
+        time.sleep
+
+print('Vous tombez face a face avec un adversaire de difficulté : force_adversaires')
+print('Vous avez quatres options')
+print('1:Combattre le monstres')
+print('2:Contourner le monstres et perdre une vie')
+print('3:Afficher les regles du jeu')
+print('4:Quitter la partie')
